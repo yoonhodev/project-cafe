9006377b5134a00a425c3b8c9859b791480b5aea
김윤호 <yhdev@gim-yunhoui-MacBookPro.local>
2022-10-26 09:53:50 +0900

    update project

--- project-template-prac/src/main/java/com/projectprac/controller/OrderController.java
+++ project-template-prac/src/main/java/com/projectprac/controller/OrderController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CartController {
+public class OrderController {
 
-	@GetMapping(path = { "cart" })
+	@GetMapping(path = { "order" })
 	public String cart() {
 		
-		return "shop/cart";
+		return "shop/order";
 	}
 }
