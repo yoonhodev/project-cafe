0fb1c7bca9a03388c5d33de25856694015138eb1
urdoon <mun7646@gmail.com>
2022-10-26 16:51:12 +0900

    update account

--- project-template-prac/src/main/java/com/projectprac/service/AccountService.java
+++ project-template-prac/src/main/java/com/projectprac/service/AccountService.java
@@ -7,5 +7,9 @@ public interface AccountService {
 	void registerCustomer(CustomerDto customer);
 
 	CustomerDto findCustomerByIdAndPasswd(String customerId, String passwd);
+
+	CustomerDto findCustomerByIdAndPhone(String customerId, String phone);
+
+	void updatePassword(String customerId, String passwd);
 	
 }
